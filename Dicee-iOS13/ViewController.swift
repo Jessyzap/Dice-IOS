91367db3c041be2bc05c3525d3a85addc11082cc
Jessica Zaparolli <jessica.zaparolli@afya.com.br>
2022-07-03 17:04:23 -0300

    initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        alphaTransition(diceImageViewOne: diceImageViewOne, diceImageViewTwo: diceImageViewTwo)
+        alphaRestore(diceImageViewOne: diceImageViewOne, diceImageViewTwo: diceImageViewTwo)
+        
     }
-
-
+    
+    func alphaTransition(diceImageViewOne: UIImageView,diceImageViewTwo: UIImageView) {
+        let transparent = 0.0
+        diceImageViewOne.alpha = transparent
+        diceImageViewTwo.alpha = transparent
+    }
+    
+    func alphaRestore (diceImageViewOne: UIImageView,diceImageViewTwo: UIImageView) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [self] in
+            
+            let opaque = 1.0
+            
+            diceImageViewOne.image = self.diceArray[Int.random(in: 0...5)]
+            diceImageViewTwo.image = self.diceArray[Int.random(in: 0...5)]
+            
+            diceImageViewOne.alpha = opaque
+            diceImageViewTwo.alpha = opaque
+        }
+        
+    }
+    
 }
-
